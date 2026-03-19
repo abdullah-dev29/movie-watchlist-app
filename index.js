240c5a2e0a70bc5c484d717031140c4641376e88
Abdullah <abdullah.01.29.2005@gmail.com>
2026-03-19 16:36:53 +0500

    Completed - fetch and render OMDb search results with full details and no-results handling

--- index.js
+++ index.js
@@ -2,25 +2,80 @@ const API_KEY = "df9e8925"
 
 const searchInputEl = document.getElementById("search-input")
 const searchBtnEl = document.getElementById("search-btn")
+const searchResultEl = document.getElementById("search-results")
 
-searchBtnEl.addEventListener("click", getSearchData)
+searchBtnEl.addEventListener("click", handleBtnClick)
+
+async function handleBtnClick() {
+  const data = await getSearchData()
+  console.log(data)
+  const html = getHtml(data)
+  console.log(html)
+  searchResultEl.style.backgroundImage = "none"
+  searchResultEl.style.backgroundColor = "#121212"
+  searchResultEl.innerHTML = html
+
+}
 
 async function getSearchData() {
   const searchTerm = searchInputEl.value;
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`)
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`)
   const data = await res.json()
 
   if (!data.Search) { // if nothing is found: undefined
-    console.log("Unable to find what you're looking for. Please try another search.")
-    return
+    return "Unable to find what you're looking for. Please try another search."
   }
   const items = data.Search
   const itemsFullData = []
   for (const item of items) {
-    const itemRes = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${item.imdbID}`)
+    const itemRes = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${item.imdbID}`)
     const itemDetails = await itemRes.json()
     itemsFullData.push(itemDetails)
   }
 
-  console.log(itemsFullData) // all complete items objects
+  return itemsFullData
+}
+
+function getHtml(data) {
+  let html = ''
+  if(typeof data === "string") {
+    html =  `
+    <div class="no-results">
+      <p>${data}</p>
+    </div>
+    `
+  } else {
+    for(const item of data) {
+      html += `
+        <div class="result">
+
+          <div class="result-poster">
+            <img src="${item.Poster}" alt="">
+          </div>
+
+          <div class="result-content">
+            <div class="result-content--title-rating">
+              <span class="title">${item.Title}</span>
+              <span class="rating">
+                <i class="fa-solid fa-star"></i>
+                ${item.imdbRating}
+              </span>
+            </div>
+            <div class="result-content--spans">
+              <span>${item.Runtime}</span>
+              <span>${item.Genre}</span>
+              <span class="watchlist" id="watchlist" data-title="${item.Title}">
+                <i class="fa-solid fa-circle-plus"></i>
+                Watchlist
+              </span>
+            </div>
+            <p>${item.Plot}</p>
+          </div>
+
+        </div>
+      `
+    }
+    
+  }
+  return html
 }
